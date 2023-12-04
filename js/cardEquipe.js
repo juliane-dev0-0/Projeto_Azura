b72dc0764583b9db253587d764596b850fcfe11f
Ja1RF <joaoreisfelini@gmail.com>
2023-12-04 16:11:09 -0300

    retirei a copia do projeto azura de dentro da pasta e coloquei redes sociais nos cards

--- js/cardEquipe.js
+++ js/cardEquipe.js
@@ -5,9 +5,21 @@ class cardEquipe extends HTMLElement {
     <img id="equipe-image"
         src="${this.getAttribute('image')}"
         alt="Card Image">
-   <div class="cardEquipe">
+    <div class="cardEquipe">
         <h1>${this.getAttribute('name')}</h1>
         <p>${this.getAttribute('text')}</p>
+        <div class="redesSociais">
+          <div class="instagramDiv">
+            <img id="instagramImg"
+              src="https://images.squarespace-cdn.com/content/v1/6019512eb9d45c40215337ea/f62e27ce-929f-46f1-8710-4d8e7cdfc2eb/logo-instagram-branco.png">
+            <h5 id="redesSociaisLinks">${this.getAttribute('arrobaInstagram')}</h5>
+          </div>
+          <div class="emailDiv">
+            <img id="emailImg"
+              src="https://static-00.iconduck.com/assets.00/mail-icon-2018x2048-0bkdtuf1.png">
+            <h5 id="redesSociaisLinks">${this.getAttribute('arrobaEmail')}</h5>
+          </div>
+        </div>
     </div>
 </div>
     `;
